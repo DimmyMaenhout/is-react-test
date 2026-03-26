a2e70a6d1842e47d10384117827a6fbe791ba1d0
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-26 16:59:52 +0100

    Added HomePage with data fetching and loading/error handling

---
+++ is-react-test/src/Pages/HomePage.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../API/posts/posts";
+import PostList from "../Components/PostList";
+
+export default function HomePage() {
+  const { data, isPending, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => fetchPosts(),
+  });
+
+  console.log(data);
+
+  if (isPending) {
+    return <p>Busy fetching posts...</p>;
+  }
+
+  if (error) {
+    return (
+      <p>
+        Something went wrong while fething the data, please try again later.
+      </p>
+    );
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <div>
+      <input placeholder="search post titles" />
+      <PostList posts={data} />
+    </div>
+  );
+}
