b002b8a8317ad0d2321dbf5a082938f1d3d21219
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-26 17:55:11 +0100

    Implemented navigation to the post details page (PostCommentsPage)

--- is-react-test/src/Components/PostItem.tsx
+++ is-react-test/src/Components/PostItem.tsx
@@ -1,14 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import type { Post } from "../Models/Post";
 
-type PostItemProps = Pick<Post, "title">;
+type PostItemProps = Pick<Post, "title" | "id">;
 
-export default function PostItem({ title }: PostItemProps) {
-  function onViewButtonClick() {}
+export default function PostItem({ title, id }: PostItemProps) {
+  const navigate = useNavigate();
+
+  function handleButtonClick() {
+    navigate(`/posts/${id}`);
+  }
 
   return (
     <li>
       <p>{title}</p>
-      <button onClick={onViewButtonClick}>View</button>
+      <button onClick={handleButtonClick}>View</button>
     </li>
   );
 }
