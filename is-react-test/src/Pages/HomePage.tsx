9826765ba23c00f3ad03beecfbd84f96cc7fed06
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-26 21:33:11 +0100

    Implemented the input search functionality

--- is-react-test/src/Pages/HomePage.tsx
+++ is-react-test/src/Pages/HomePage.tsx
@@ -1,14 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchPosts } from "../API/posts/posts";
 import PostList from "../Components/PostList";
+import { useState, type ChangeEvent } from "react";
 
 export default function HomePage() {
+  const [search, setSearch] = useState<string>("");
+
   const { data, isPending, error } = useQuery({
     queryKey: ["posts"],
     queryFn: () => fetchPosts(),
   });
 
-  console.log(data);
+  function handleSearchInput(e: ChangeEvent<HTMLInputElement>) {
+    setSearch(e.target.value);
+  }
 
   if (isPending) {
     return <p>Busy fetching posts...</p>;
@@ -26,10 +31,21 @@ export default function HomePage() {
     return <p>No posts found.</p>;
   }
 
+  const filteredData = data.filter((post) =>
+    post.title.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
-    <div>
-      <input placeholder="search post titles" />
-      <PostList posts={data} />
-    </div>
+    <main>
+      <div>
+        <input
+          type="text"
+          onChange={handleSearchInput}
+          placeholder="search post titles"
+          value={search}
+        />
+      </div>
+      <PostList posts={filteredData} />
+    </main>
   );
 }
