f0e6a7aaa6eeaf97f2dc2a812b37f705c66d9a39
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-26 18:02:12 +0100

    Added PostCommentsPage, fetch comments and handle loading, error & empty states

--- is-react-test/src/App.tsx
+++ is-react-test/src/App.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
+import PostCommentsPage from "./Pages/PostCommentsPage";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/posts/:id", element: <PostCommentsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
---
+++ is-react-test/src/Pages/PostCommentsPage.tsx
@@ -0,0 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { fetchPostComments } from "../API/posts/posts";
+
+export default function PostCommentsPage() {
+  const { id } = useParams<{ id: string }>();
+
+  if (!id) {
+    return <p>Invalid post id</p>;
+  }
+
+  const {
+    data: comments,
+    isPending,
+    error,
+  } = useQuery({
+    queryKey: ["post", id, "comments"],
+    queryFn: () => fetchPostComments(id),
+    enabled: !!id,
+  });
+
+  function onCloseClick() {}
+
+  if (isPending) {
+    return <p>Fetching post comments...</p>;
+  }
+
+  if (error) {
+    return (
+      <p>
+        Something went wrong while fething the post comments, please try again
+        later.
+      </p>
+    );
+  }
+
+  if (!comments?.length) {
+    return <p>No post comments found.</p>;
+  }
+
+  return (
+    <div>
+      <button onClick={onCloseClick}>Close</button>
+
+      <h2>Comments</h2>
+      <ul>
+        {comments.map((comment) => {
+          return <li key={comment.id}>{comment.body}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
